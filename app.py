75f1988b33e93b38d1d9b6263d68c24359adc08f
Arie27R <52940197+Arie27R@users.noreply.github.com>
2024-06-26 20:43:24 -0500

    version 2

--- app.py
+++ app.py
@@ -3,19 +3,18 @@ import scipy.stats
 import streamlit as st
 import time
 
-#estas son variables de estado que se conservan cuando Streamlin vuelve a ejecutar este script
-
+# estas son variables de estado que se conservan cuando Streamlin vuelve a ejecutar este script
 if 'experiment_no' not in st.session_state:
-    st.session_state['exoerinment_no'] = 0
+    st.session_state['experiment_no'] = 0
 
-if 'df_experiment_result' not in st.session_state:
-    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iteraciones', 'media'])    
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iteraciones', 'media'])
 
 st.header('Lanzar una moneda')
 
 chart = st.line_chart([0.5])
 
-def toss_coin(n): #función que emula el lanzamiento de una moneda
+def toss_coin(n):
 
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
@@ -27,9 +26,9 @@ def toss_coin(n): #función que emula el lanzamiento de una moneda
         outcome_no +=1
         if r == 1:
             outcome_1_count += 1
-            mean = outcome_1_count / outcome_no
-            chart.add_rows([mean])
-            time.sleep(0.05)
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
 
     return mean
 
@@ -50,4 +49,4 @@ if start_button:
         axis=0)
     st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
 
-st.write('Esta aplicación aún no es fncional. En construcción')
+st.write(st.session_state['df_experiment_results'])
